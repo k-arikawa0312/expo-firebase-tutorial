9213c8aabe1d3c72240bc7f963d8da785c6dc175
k-arikawa0312 <s1F102402710@iniad.org>
2025-01-14 18:07:07 +0900

    Install expo-router

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -18,4 +18,4 @@ export default [
       "comma-dangle": [2, "never"]
     }
   }
-];
+]
