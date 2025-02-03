e97bd29092dc9e94896abf3d8aebc366c8181df3
k-arikawa0312 <s1F102402710@iniad.org>
2025-02-03 19:54:13 +0900

    styling login

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, TextInput } from "react-native"
 import Header from "../../components/Header"
 
 const Login = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <Header />
-            <View>
-                <Text>Login</Text>
+            <View style={styles.inner}>
+                <Text style={styles.title}>Login</Text>
+                <TextInput style={styles.input} value="Email Address" />
+                <TextInput style={styles.input} value="Password" />
+            </View>
+            <View style={styles.button}>
+                <Text style={styles.buttonLabel}>Submit</Text>
+            </View>
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>Not registered?</Text>
+                <Text style={styles.footerLink}>Sign up here!</Text>
             </View>
         </View>
     )
@@ -14,7 +23,53 @@ const Login = (): JSX.Element => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#F0F4F8'
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginBottom: 24
+    },
+    inner: {
+        paddingVertical: 24,
+        paddingHorizontal: 27
+    },
+    input: {
+        backgroundColor: '#dddddd',
+        borderWidth: 1,
+        borderColor: '#ffffff',
+        height: 48,
+        padding: 8,
+        fontSize: 16,
+        marginBottom: 16
+    },
+    button: {
+        backgroundColor: '#467FD3',
+        borderRadius: 4,
+        alignSelf: 'flex-start',
+        marginBottom: 24
+    },
+    buttonLabel: {
+        fontSize: 16,
+        lineHeight: 32,
+        color: '#ffffff',
+        paddingVertical: 8,
+        paddingHorizontal: 24
+    },
+    footer: {
+        flexDirection: 'row'
+    },
+    footerText: {
+        fontSize: 14,
+        lineHeight: 24,
+        marginRight: 8
+    },
+    footerLink: {
+        fontSize: 14,
+        lineHeight: 24,
+        color: '#467FD3'
     }
 })
 
