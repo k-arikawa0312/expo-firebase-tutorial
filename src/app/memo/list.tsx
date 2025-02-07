fad255bda37e4c38e17e506eb711416b3b314388
k-arikawa0312 <s1F102402710@iniad.org>
2025-02-08 05:46:31 +0900

    memo型のついか

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -3,16 +3,18 @@ import MemoListItem from "../../components/MemoListItem"
 import CircleButton from "../../components/CircleButton"
 import Icon from "../../components/Icon"
 import { router, useNavigation } from "expo-router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import LogOutButton from "../../components/LogOutButton"
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
 import { auth, db } from "../../config"
+import { Memo } from "../../../types/memo"
 
 const handlePress = (): void => {
     router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
+    const [memos, setMemos] = useState<Memo[]>([])
     const navigation = useNavigation()
     useEffect(() => {
         navigation.setOptions({
@@ -23,9 +25,17 @@ const List = (): JSX.Element => {
         const ref = collection(db, `users/${auth.currentUser?.uid}/memos`)
         const q = query(ref, orderBy('updatedAt', 'desc'))
         const unsubscribe = onSnapshot(q, (snapshot) => {
+            const remoteMemos: Memo[] = []
             snapshot.forEach((doc) => {
                 console.log('memo', doc.data())
+                const { bodyText, updatedAt } = doc.data()
+                remoteMemos.push({
+                    id: doc.id,
+                    bodyText,
+                    updatedAt
+                })
             })
+            setMemos(remoteMemos)
         })
         return unsubscribe
     }, [])
