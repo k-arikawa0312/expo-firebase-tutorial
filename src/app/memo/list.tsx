f289f257efc99c1fa7bd44ebe023da61c48615b6
k-arikawa0312 <s1F102402710@iniad.org>
2025-02-08 05:56:32 +0900

    memoの一覧表示

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -42,9 +42,7 @@ const List = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <View>
-                <MemoListItem/>
-                <MemoListItem/>
-                <MemoListItem/>    
+                {memos.map((memo) => <MemoListItem memo={memo} />)}
             </View>
             <CircleButton onPress={handlePress}>
                 <Icon name="add" size={40} color="#ffffff"/>
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,14 +1,23 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 import Icon from "./Icon"
 import { Link } from "expo-router"
+import { Memo } from "../../types/memo"
 
-const MemoListItem = (): JSX.Element => {
+interface Props {
+    memo: Memo
+}
+
+const MemoListItem = (props: Props): JSX.Element => {
+    const { memo } = props
+    const { bodyText, updatedAt } = memo
+    if (bodyText === null || updatedAt === null) return <Text>メモがありません</Text>
+    const dateString =updatedAt.toDate().toLocaleString('ja-JP')
     return (
         <Link href='/memo/detail' asChild>
             <TouchableOpacity style={styles.memoListItem}>
                 <View>
-                    <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                    <Text style={styles.memoListItemDate}>時間</Text>
+                    <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+                    <Text style={styles.memoListItemDate}>{dateString}</Text>
                 </View>
                 <TouchableOpacity>
                     <Icon name="close" size={32} color="#B0B0B0"/>
